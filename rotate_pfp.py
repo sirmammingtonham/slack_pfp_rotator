ffd6fcd88ab24345b52263cab3badeca84d3e3ee
Ethan Joseph <ethan@xyla.com>
2025-04-30 00:40:42 -0400

    Refactor avatar rotation logic to use date ordinal for image selection

--- rotate_pfp.py
+++ rotate_pfp.py
@@ -1,6 +1,7 @@
 import os
 from slack_sdk import WebClient
 from pathlib import Path
+import datetime
 
 # Load your token from an env var
 SLACK_TOKEN = os.environ["SLACK_USER_TOKEN"]
@@ -8,16 +9,17 @@ client = WebClient(token=SLACK_TOKEN)
 
 # Point this at a folder of pre-prepared square images
 IMAGES_DIR = Path("./squiddy")
-START_OFFSET = 2
+START_OFFSET = 2 # started on apr 30th
 
 
 def set_daily_avatar():
     # Pick today’s image (e.g. based on day-of-year)
     images = sorted(IMAGES_DIR.glob("*.png"))
-    idx = int(
-        ((Path().resolve().stat().st_mtime // 86400) + START_OFFSET) % len(images)
-    )
+    today = datetime.date.today()
+    day_count = today.toordinal() - datetime.date(1970, 1, 1).toordinal()
+    idx = (day_count + START_OFFSET) % len(images)
     img_path = images[idx]
+
     with img_path.open("rb") as img:
         client.users_setPhoto(image=img)
     print(f"Set avatar to {img_path.name!r}")
